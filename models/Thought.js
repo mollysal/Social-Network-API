141eceb91f6b60dfefbff0f238ef9871d21d82dc
Molly Salmonsen <salmonsen.molly@gmail.com>
2022-11-08 20:50:18 -0500

    Updating Formatting & Thought Model

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const { format_date, format_time } = require('../utils/helper');
+
+// Schema to create Student model
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createAt: {
+            type: Date,
+            default: Date.now(),
+            get: format_time,
+            // Use a getter method to format the timestamp on query
+            required: true,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+// Virtual retrieves the length of the user's friends array field on query.
+// Reference Activity 21
+userSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+})
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -3,38 +3,38 @@ const reactionSchema = require('./Reaction');
 
 // Schema to create Student model
 const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      unique: true,
-      required: true,
-      trim: true,
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Please enter a valid email address."]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectID,
+                ref: "thought",
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectID,
+                ref: "user",
+            }
+        ]
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Please enter a valid email address."]
-    },
-    thoughts: [
-        {
-            type: Schema.Types.ObjectID,
-            ref: "thought",
-        }
-    ],
-    friends: [
-        {
-            type: Schema.Types.ObjectID,
-            ref: "user",
-        }
-    ]
-  },
-  {
-    toJSON: {
-    virtuals: true,
-      getters: true,
-    },
-  }
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+    }
 );
 
 // Virtual retrieves the length of the user's friends array field on query.
